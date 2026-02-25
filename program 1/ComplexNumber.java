1048c83b084f2d22c49d3f02fd75adaea1655d43
ameera_basheer <0ameerabasheer24@gmail.com>
2026-02-25 10:06:56 +0530

    Add files via upload

---
+++ program 1/ComplexNumber.java
@@ -0,0 +1,40 @@
+public class ComplexNumber{
+double real,img;
+
+ComplexNumber(double r,double i) {
+real = r; 
+img = i; 
+}
+
+public static ComplexNumber sum(ComplexNumber c1,ComplexNumber c2)
+{
+ComplexNumber temp = new ComplexNumber (0,0);
+temp.real = c1.real + c2.real;
+temp.img = c1.img + c2.img;
+return temp;
+}
+
+public static void main(String args[])
+{
+java.util.Scanner sc = new java.util.Scanner(System.in);
+
+System.out.print("Enter real part of first complex number: ");
+double r1 = sc.nextDouble();
+
+System.out.print("Enter imaginary part of first complex number: ");
+double i1 = sc.nextDouble();
+
+System.out.print("Enter real part of second complex number: ");
+double r2 = sc.nextDouble();
+
+System.out.print("Enter imaginary part of second complex number: ");
+double i2 = sc.nextDouble();
+
+ComplexNumber c1 = new ComplexNumber(r1,i1);
+ComplexNumber c2 = new ComplexNumber(r2,i2);
+
+ComplexNumber temp = sum(c1,c2);
+
+System.out.printf("Sum is :" + temp.real + "+" + temp.img + "i");
+}
+}
